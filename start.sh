372bcf8e58da95dfce68855fa150a8224c4a660f
InterAnylastics <72003733+InternetAmethyst@users.noreply.github.com>
2021-07-23 12:44:26 +0530

    strt

--- start.sh
+++ start.sh
@@ -1,18 +1,3 @@
-echo "Configuring Andencento" # Bisi Dont Kang Made by Noob Stranger if wanna kang then first ask else get ready to get copyright strike based in DMCA
-if [[ $UPSTREAM_REPO == "DEV" ]]
-then
-   git clone -b Dev https://github.com/Andencento/Andencento.git
-else
-   git clone https://github.com/Andencento/Andencento.git
-fi
-cd Andencento
-
-echo "
-   / \   _ __   __| | ___ _ __   ___ ___ _ __ | |_ ___  
-  / _ \ | '_ \ / _ |/ _ \ '_ \ / / _ \ '_ \| / _ \    | 
- / ___ \| | | | (_| |  __/ | | | (_|  __/ | | | || (_) | 
-/_/   \_\_| |_|\__,_|\___|_| |_|\___\___|_| |_|\__\___/                      
-
-"
+pip install Andencento-Userbot
 
 python3 -m userbot
